12b32cd4c46ca9744a59abfa1aec8eed39bc3180
Andrew Mozolev <andrew.mozolev@gmail.com>
2025-01-08 09:34:27 +1300

    Enhance MPEG-TS parser to locate the first sync byte

--- mpegts-parser.js
+++ mpegts-parser.js
@@ -11,12 +11,41 @@ let numberOfPackets = 0;
 let prevChunk;
 
 process.stdin.on('data', (chunk) => {
+  let startIndex = 0;
+
   if (prevChunk) {
     chunk = Buffer.concat([prevChunk, chunk]);
     prevChunk = undefined;
   }
 
-  for (let i = 0; i < chunk.length; i += PACKET_SIZE_BYTES) {
+  if (numberOfPackets === 0) {
+    // Find the first sync byte in the chunk
+    let isSyncByteFound = false;
+
+    for (let j = 0; j < PACKET_SIZE_BYTES; j++) {
+      const syncByte = chunk[j];
+      const secondSyncByte = chunk[j + PACKET_SIZE_BYTES];
+
+      if (syncByte === SYNC_BYTE && secondSyncByte === SYNC_BYTE) {
+        isSyncByteFound = true;
+        startIndex = j;
+        break;
+      }
+    }
+
+    if (!isSyncByteFound) {
+      const offset = numberOfPackets * PACKET_SIZE_BYTES;
+
+      console.error(
+        `Error: No sync byte present in packet ${numberOfPackets}, offset ${offset}`
+      );
+
+      process.exit(1);
+    }
+  }
+
+  // Parse the rest of the packets
+  for (let i = startIndex; i < chunk.length; i += PACKET_SIZE_BYTES) {
     if (i + PACKET_SIZE_BYTES >= chunk.length) {
       if (numberOfPackets === 0) {
         console.error(
